ed9413aa06da7da9e4a38f2911d3a9a38f8812e6
mazy7c8 <rafalinz@gmail.com>
2020-03-25 13:30:27 +0100

    template-setup

--- main/urls.py
+++ main/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('landingpage.urls'))
 ] + staticfiles_urlpatterns()
 
 if settings.DEBUG:
