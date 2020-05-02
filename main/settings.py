aa042261afa1533b9f64cce00c5bd92992a0734f
NotJustPizza <notjustpizza@dmailo.com>
2020-05-03 00:31:43 +0200

    Removed Django admin

--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,6 @@ INTERNAL_IPS = env('INTERNAL_IPS', default=['127.0.0.1'])
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
